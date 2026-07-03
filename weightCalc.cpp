5884b938f08aa4f52397ebfbd7771044d59182d6
winkinsss <realwinkins@gmail.com>
2026-07-03 05:03:20 +0300

    first commit

---
+++ weightCalc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+ double weight;
+ double weightm;
+ std::cout<<"How much is the weight of your item: ";
+ std::cin>> weight;
+ weightm = 0.38*weight;
+ std::cout<<"The weight of the item on mars is: "<< weightm;
+ 
+}
